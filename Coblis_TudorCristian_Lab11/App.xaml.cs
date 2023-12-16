fa1305c997d8f7ba221d648d13c8ee0d512154e5
TUDOR-CRISTIAN COBLIȘ <tudor.coblis@stud.ubbcluj.ro>
2023-12-16 16:43:02 +0200

    Add project files.

---
+++ Coblis_TudorCristian_Lab11/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Coblis_TudorCristian_Lab11.Data;
+
+namespace Coblis_TudorCristian_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Coblis_TudorCristian_Lab11/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Coblis_TudorCristian_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
